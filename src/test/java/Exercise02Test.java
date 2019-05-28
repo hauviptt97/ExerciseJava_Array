584c4c4e2585479eb475f814ce91691f6f05717e
HauBui <hauviptt@gmail.com>
2019-05-28 14:26:38 +0700

    exercise 02

---
+++ src/test/java/Exercise02Test.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Exercise02Test {
+
+    @Test
+    public void test_sumOfThreeGreatestNumbers() {
+        Assert.assertEquals(new Exercise02().sumOfThreeGreatestNumbers(new Integer[]{1, 1, 3, 1, 5, 6, 1, 3}), 14);
+        Assert.assertEquals(new Exercise02().sumOfThreeGreatestNumbers(new Integer[]{1, 4}), 0);
+    }
+}
