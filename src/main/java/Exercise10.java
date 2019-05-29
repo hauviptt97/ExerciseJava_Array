442d8d5bda0b0275f41e1597b86e9142f4513fb6
HauBui <hauviptt@gmail.com>
2019-05-29 14:23:42 +0700

    exercise 10

---
+++ src/main/java/Exercise10.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exercise10 {
+
+    public String split(int[] numbers, int n) {
+
+        List<List<Integer>> subArrays = new ArrayList<>();
+
+        int lengthOfSubArray = numbers.length % n == 0 ? numbers.length / n : numbers.length / n + 1;
+
+        List<Integer> temp = new ArrayList<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            temp.add(numbers[i]);
+            if (temp.size() == lengthOfSubArray) {
+                subArrays.add(temp);
+                temp = new ArrayList<>();
+            }
+
+        }
+
+        if (numbers.length % n != 0) {
+            subArrays.add(temp);
+        }
+
+        return subArrays.toString();
+    }
+
+}
