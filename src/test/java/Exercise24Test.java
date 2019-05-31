f22f5a5cf0199e5fdb2b4380a431edf5ed5a7337
HauBui <hauviptt@gmail.com>
2019-05-31 17:15:09 +0700

    exercise 24

--- src/test/java/Exercise24Test.java
+++ src/test/java/Exercise24Test.java
@@ -5,5 +5,6 @@ public class Exercise24Test {
     @Test
     public void test_findLCMOfArray() {
         Assert.assertEquals(new Exercise24().findLCMOfArray(new int[]{3, 6, 9}), 18);
+        Assert.assertEquals(new Exercise24().findLCMOfArray(new int[]{3, 6, 9, 0}), 0);
     }
 }
