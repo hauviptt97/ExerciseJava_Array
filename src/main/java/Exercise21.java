d6558cf6085fbd59be2aa2fc89574856b25dea76
HauBui <hauviptt@gmail.com>
2019-05-31 14:58:40 +0700

    exercise-21

--- src/main/java/Exercise21.java
+++ src/main/java/Exercise21.java
@@ -2,6 +2,19 @@ public class Exercise21 {
 
     public int caculateDet(int[][] numbers) {
 
+        if (numbers.length != numbers[0].length) {
+            return 0;
+        }
+
+        if (numbers.length == 1) {
+            return numbers[0][0];
+        }
+
+        if (numbers.length == 2) {
+            return numbers[0][0] * numbers[1][1] - numbers[1][0] * numbers[0][1];
+        }
+
+
         int det = 0;
 
         for (int i = 0; i < numbers.length; i++) {
@@ -9,8 +22,10 @@ public class Exercise21 {
             int b = 1;
 
             for (int x = i, y = 0; y < numbers.length; x = (x + 1) % numbers.length, y++) {
+
                 a *= numbers[y][x];
                 b *= numbers[numbers.length - 1 - y][x];
+
             }
 
             det += a - b;
---
+++ src/test/java/Exercise21Test.java
@@ -0,0 +1,22 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Exercise21Test {
+    @Test
+    public void test_caculateDet() {
+        Assert.assertEquals(new Exercise21().caculateDet(new int[][]{
+                {-2, 2, -3},
+                {-1, 1, 3},
+                {2, 0, -1}
+        }), 18);
+
+        Assert.assertEquals(new Exercise21().caculateDet(new int[][]{
+                {2, 2},
+                {3, 4}
+        }), 2);
+
+        Assert.assertEquals(new Exercise21().caculateDet(new int[][]{
+                {1}
+        }), 1);
+    }
+}
