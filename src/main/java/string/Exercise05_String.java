8501c43d4249eeaf35bca7200545b018474fecc9
HauBui <hauviptt@gmail.com>
2019-06-03 14:05:57 +0700

    exercise 05 string

--- src/main/java/string/Exercise05_String.java
+++ src/main/java/string/Exercise05_String.java
@@ -1,51 +1,42 @@
 package string;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Exercise05_String {
 
-    private String[] getWords(String input) {
+    private List<String> getWords(String input) {
+        List<String> words = new ArrayList<>();
 
         if (!input.trim().isEmpty()) {
 
             String[] temp = input.split("[ ]+");
 
-            String[] words = new String[temp.length];
-            int count = 0;
-
             for (String word : temp) {
                 if (word.matches("[\\w]+")) {
-                    words[count++] = word;
+                    words.add(word);
                 }
             }
-            return words;
         }
-        return null;
+        return words;
     }
 
     public List<String> getDuplicateWords(String input) {
 
-        String[] words = getWords(input);
-
-        if (words != null) {
+        List<String> words = getWords(input);
+        if (words.size() > 1) {
             ArrayList<String> outputArray = new ArrayList<>();
 
-            String current = words[0];
+            Collections.sort(words);
 
-            int count = 1;
-
-            Arrays.sort(words);
-
-            for (int i = 0; i < words.length - 1; i++) {
-                if (words[i].equals(words[i + 1])) {
-                    outputArray.add(words[i]);
+            for (int i = 0; i < words.size() - 1; i++) {
+                if (words.get(i).equals(words.get(i + 1))) {
+                    outputArray.add(words.get(i));
                 }
             }
-
             return outputArray;
         }
-        return null;
+        return words;
     }
 }
--- src/test/java/string/Exercise05_String_Test.java
+++ src/test/java/string/Exercise05_String_Test.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class Exercise05_String_Test {
     @Test
     public void test_getDuplicateWords(){
-        Assert.assertEquals(new Exercise05_String().getDuplicateWords("hau bui is the best hau ga ga"), Arrays.asList("ga","hau"));
-        Assert.assertEquals(new Exercise05_String().getDuplicateWords("  "), null);
+        Assert.assertEquals(new Exercise05_String().getDuplicateWords("hau bui! is the best hau ga ga"), Arrays.asList("ga","hau"));
+        Assert.assertEquals(new Exercise05_String().getDuplicateWords("hau. bui!"),Arrays.asList());
     }
 }
