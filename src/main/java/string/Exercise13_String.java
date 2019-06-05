a2b2e340f02bbb17ec68daa5ec4214a3555c3bba
HauBui <hauviptt@gmail.com>
2019-06-05 09:52:57 +0700

    Exercise 13 string

--- src/main/java/string/Exercise13_String.java
+++ src/main/java/string/Exercise13_String.java
@@ -5,13 +5,27 @@ public class Exercise13_String {
 
     public boolean isEqual(String input1, String input2) {
 
-        System.out.println(input1.compareTo(input2));
+        String str1 = input1.toLowerCase();
+        String str2 = input2.toLowerCase();
 
-        System.out.println(input1);
-        return true;
-    }
+        if (input1.length() != input2.length()) {
+            return false;
+        }
 
-    public static void main(String[] args) {
-        System.out.println(new Exercise13_String().isEqual("cxzczcxzc", "cxzczczxc"));
+        int indexEq;
+
+        for (int i = 0; i < str2.length(); i++) {
+
+            indexEq = str1.indexOf(str2.charAt(i));
+
+            if (indexEq == -1) {
+                return false;
+            }
+
+            str1 = str1.replace(str2.charAt(i), ' ');
+            str2 = str2.replace(str2.charAt(i), ' ');
+        }
+
+        return str1.equals(str2);
     }
 }
--- src/test/java/string/Exercise13_String_Test.java
+++ src/test/java/string/Exercise13_String_Test.java
@@ -1,4 +1,15 @@
 package string;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Exercise13_String_Test {
+
+    @Test
+    public void test_isEqual(){
+        Assert.assertEquals(new Exercise13_String().isEqual("qazczcxzc", "cxzazczxQ"),true);
+        Assert.assertEquals(new Exercise13_String().isEqual("qazczcxzc", "cxzazczxQc"),false);
+        Assert.assertEquals(new Exercise13_String().isEqual("qazczcxAc", "cxzazczxQ"),true);
+        Assert.assertEquals(new Exercise13_String().isEqual("qazczcxAc", "cxzazczxH"),false);
+    }
 }
