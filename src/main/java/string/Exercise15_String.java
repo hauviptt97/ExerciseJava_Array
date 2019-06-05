618c800ab81d2ee7391d2532f524a8a9bcee65f9
HauBui <hauviptt@gmail.com>
2019-06-05 10:55:40 +0700

    Exercise 15 string

--- src/main/java/string/Exercise15_String.java
+++ src/main/java/string/Exercise15_String.java
@@ -2,23 +2,9 @@ package string;
 
 public class Exercise15_String {
 
-    public boolean isValidBarcode(String barcode) {
-        if (barcode.length() != 13){
-            return false;
-        }
-
-        int sum = 0;
-
-        for (int i = 0; i < barcode.length(); i++) {
-            sum += ((i & 1) * 2 + 1) * Character.getNumericValue(barcode.charAt(i));
-        }
-
-        return sum % 10 == 0;
-    }
-
     public int getVerifyNumber(String barcode) {
 
-        if (barcode.length() != 12) {
+        if (barcode.length() != 12 || barcode.matches("[\\D]+")) {
             return -1;
         }
 
