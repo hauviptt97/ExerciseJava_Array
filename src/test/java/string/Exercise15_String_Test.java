ff0b7b95ce00373ef73f47153ce42bcf1d8808d1
HauBui <hauviptt@gmail.com>
2019-06-05 11:09:59 +0700

    Exercise 15 string

--- src/test/java/string/Exercise15_String_Test.java
+++ src/test/java/string/Exercise15_String_Test.java
@@ -8,14 +8,13 @@ public class Exercise15_String_Test {
     @Test
     public void test_getVerifyNumber() {
         Assert.assertEquals(new Exercise15_String().getVerifyNumber("893850597419"), 4);
-        Assert.assertEquals(new Exercise15_String().getVerifyNumber("89385059741941"), -1);
-        Assert.assertEquals(new Exercise15_String().getVerifyNumber("8938505974"), -1);
-        Assert.assertEquals(new Exercise15_String().getVerifyNumber("89385t5974194"), -1);
-        Assert.assertEquals(new Exercise15_String().getVerifyNumber("8938505274194"), -1);
-        Assert.assertEquals(new Exercise15_String().getVerifyNumber("893850527.19s"), -1);
-        Assert.assertEquals(new Exercise15_String().getVerifyNumber("zxczxczxczxcc"), -1);
-        Assert.assertEquals(new Exercise15_String().getVerifyNumber("zxczxczxcc"), -1);
-        Assert.assertEquals(new Exercise15_String().getVerifyNumber("zxczxczxccwqeqwd"), -1);
+        Assert.assertEquals(new Exercise15_String().getVerifyNumber("8385059741941"), -1);
+        Assert.assertEquals(new Exercise15_String().getVerifyNumber("838505974"), -1);
+        Assert.assertEquals(new Exercise15_String().getVerifyNumber("8385597194"), -1);
+        Assert.assertEquals(new Exercise15_String().getVerifyNumber("83850527.19s"), -1);
+        Assert.assertEquals(new Exercise15_String().getVerifyNumber("zczxczxczxcc"), -1);
+        Assert.assertEquals(new Exercise15_String().getVerifyNumber("zczxczxcc"), -1);
+        Assert.assertEquals(new Exercise15_String().getVerifyNumber("zczxczxccwqeqwd"), -1);
     }
 
 }
