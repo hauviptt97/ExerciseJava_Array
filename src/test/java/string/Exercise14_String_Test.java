c0a4b2047aa123dcc3ce84ccbb107221eefdb5ce
HauBui <hauviptt@gmail.com>
2019-06-05 10:18:22 +0700

    Exercise 14 string

--- src/test/java/string/Exercise14_String_Test.java
+++ src/test/java/string/Exercise14_String_Test.java
@@ -11,5 +11,6 @@ public class Exercise14_String_Test {
         Assert.assertFalse(new Exercise14_String().isValidBarcode("89385059741941"));
         Assert.assertFalse(new Exercise14_String().isValidBarcode("89385t5974194"));
         Assert.assertFalse(new Exercise14_String().isValidBarcode("8938505274194"));
+        Assert.assertFalse(new Exercise14_String().isValidBarcode("893850527.19s"));
     }
 }
