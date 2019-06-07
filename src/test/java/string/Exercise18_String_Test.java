583b64cca178481764e2502d03a62ab7135efd78
HauBui <hauviptt@gmail.com>
2019-06-07 15:21:22 +0700

    Exercise 18 string

--- src/test/java/string/Exercise18_String_Test.java
+++ src/test/java/string/Exercise18_String_Test.java
@@ -10,6 +10,7 @@ public class Exercise18_String_Test {
         Assert.assertEquals(new Exercise18_String().multiply("31335", "2241"),"70221735");
         Assert.assertEquals(new Exercise18_String().multiply("-2241", "31335"),"-70221735");
         Assert.assertEquals(new Exercise18_String().multiply("2241", "-31335"),"-70221735");
+        Assert.assertEquals(new Exercise18_String().multiply("-2241", "-31335"),"70221735");
         Assert.assertEquals(new Exercise18_String().multiply("2--241", "31335"),"Invalid Number");
         Assert.assertEquals(new Exercise18_String().multiply("2241", "3-13j35"),"Invalid Number");
         Assert.assertEquals(new Exercise18_String().multiply("000000", "-31335"),"0");
